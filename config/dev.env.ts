e69f7067a596fee0e523b977e2588dae33c7ebb9
xue ming <810505339@qq.com>
2023-09-20 21:33:26 +0800

    init

--- config/dev.env.ts
+++ config/dev.env.ts
@@ -2,13 +2,13 @@
 export default {
   NODE_ENV: "development",
   // 业务服务器地址
-  CHAT_URL: "http://43.128.72.19:10008",
+  CHAT_URL: "http://114.67.231.163:10008",
   // IM api地址
-  API_URL: "http://43.128.72.19:10002",
+  API_URL: "http://114.67.231.163:10002",
   // IM 配置获取地址
-  CONFIG_URL: "http://43.128.72.19:10009",
-  // IM websocket地址
-  WS_URL: "ws://43.128.72.19:10001",
+  CONFIG_URL: "http://114.67.231.163:10009",
+  // IM websocket地址/*  */
+  WS_URL: "ws://114.67.231.163:10001",
   // 高德地图key
   AMAP_KEY: "",
 };
