d5f41bd2f5c3cadfe624cd898de96cc18771a99d
xueming <810505339@qq.com>
2023-09-21 15:43:18 +0800

    edit配置

--- config/prod.env.ts
+++ config/prod.env.ts
@@ -6,7 +6,7 @@ export default {
   // IM api地址
   API_URL: "http://114.67.231.163:10002",
   // IM 配置获取地址
-  CONFIG_URL: "http://114.67.231.163:10009",
+  CONFIG_URL: "http://114.67.231.163:10008",
   // IM websocket地址/*  */
   WS_URL: "ws://114.67.231.163:10001",
   // 高德地图key
